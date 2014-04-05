afb13fa87babc85d957bde0b1ab9457eff4937e8
Joseph Fahey <fahey.joseph@gmail.com>
2014-04-05 22:19:58 +0200

    include examples from acquainted post

--- src/info/josf/examples/acquainted.clj
+++ src/info/josf/examples/acquainted.clj
@@ -1,6 +1,24 @@
-(ns info.josf.examples.acquainted)
+(ns info.josf.examples.acquainted
+  "Functions used as examples for my first post about Clojure zippers:b
+  http://josf.info/blog/2014/03/21/getting-acquainted-with-clojure-zippers/"
+  (:require [clojure.zip :as zip]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def zzz (zip/vector-zip [1 [:a :b] 2 3 [40 50 60]]))
+
+(defn fast-forward [loc]
+	(if (zip/end? loc)
+	  (zip/node loc)
+	  (recur (zip/next loc))))
+
+ (defn fast-forward-z [loc]
+	(if (zip/end? loc)
+	  loc
+	  (recur (zip/next loc))))
+
+ (defn first-keyword [loc]
+	(if (zip/end? loc)
+	  nil
+	  (if (keyword? (zip/node loc))
+	    (zip/node loc)
+	    (recur (zip/next loc)))))
+

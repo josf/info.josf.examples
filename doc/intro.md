cb187783937f4bf12173033f5fe457105de61083
Joseph Fahey <fahey.joseph@gmail.com>
2014-04-05 21:58:55 +0200

    initial setup
    - gitig
    - data.zip

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to info.josf.examples
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/info/josf/examples_test.clj
@@ -0,0 +1,7 @@
+(ns info.josf.examples-test
+  (:require [clojure.test :refer :all]
+            [info.josf.examples :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

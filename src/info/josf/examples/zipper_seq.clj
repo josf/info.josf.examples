b4cc4f0b1272fb42a6a8e32417b7bfb0da77e5a6
Joseph Fahey <fahey.joseph@gmail.com>
2014-04-13 21:53:49 +0200

    most of 3rd zipper article examples

--- src/info/josf/examples/zipper_seq.clj
+++ src/info/josf/examples/zipper_seq.clj
@@ -32,15 +32,54 @@
 
 (def zzz (zip/vector-zip [:a [:b [:c :d] :e]]))
 
-;; grab a webpage and parse it into a zipper
-(def zappy
-     (-> "http://richhickey.github.io/clojure/clojure.zip-api.html"
-     	  slurp
-	  xml/parse-str
-	  zip/xml-zip))
+(defn zappy
+  "grab a webpage we can trust, and parse it into a zipper"
+  []
+  (-> "http://richhickey.github.io/clojure/clojure.zip-api.html"
+      clojure.java.io/input-stream
+      dx/parse
+      zip/xml-zip))
 
 (defn get-links [loc]
   (map #(get-in (zip/node %) [:attrs :href])
        (filter #(= :a (:tag (zip/node %))) 
                (take-while (complement zip/end?) 
                            (iterate zip/next loc)))))
+
+(defn get-a-zips
+  "Return a lazy seq of :a locations"
+  [loc]
+  (filter #(= :a (:tag (zip/node %)))
+          (take-while (complement zip/end?)
+                      (iterate zip/next loc))))
+
+(defn ancestor-count
+  "How many ancestors do I have?"
+  [loc]
+  (count (take-while zip/up
+                     (iterate zip/up loc))))
+
+(defn descendant-count
+  "How many descendants do I have?"
+  [loc]
+  (let [sub-zip (zip/xml-zip (zip/node loc))]
+    (count (take-while (complement zip/end?)
+                       (iterate zip/next (zip/down sub-zip))))))
+
+(defn extract-url
+  "Get the href value from an <a> zipper. Takes a loc."
+  [loc]
+  (get-in (zip/node loc) [:attrs :href]))
+
+
+(defn depth-href-kidcount
+  "loc-seq is a seq of <a> node zipper locations."
+  [loc-seq]
+  (map
+   (fn [loc]
+    {:depth (ancestor-count loc)
+     :href (extract-url loc)
+     :subnodes (descendant-count loc)})
+   loc-seq))
+
+
